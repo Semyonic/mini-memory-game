f930747badacc228c667bc71a859cbeb34ee66ea
Semih Onay <6043982+Semyonic@users.noreply.github.com>
2019-07-10 10:21:37 +0300

    Update ResultView.js

--- src/views/results/ResultView.js
+++ src/views/results/ResultView.js
@@ -1,25 +1,27 @@
 import React from "react";
-import { initGame } from "../../state/actions/game.actions";
+import { withRouter } from 'react-router-dom';
 
-export default class ResultView extends React.Component {
+class ResultView extends React.Component {
   constructor(props) {
     super(props);
     this.restart = this.restart.bind(this);
+    this.result = this.props.history.location.state.result;
   }
 
   restart() {
-    this.props.history.push("/");
-    this.dispatch(initGame());
+    this.props.history.push('/');
   }
   render() {
     return (
       <div className="status" style={{ textAlign: "center", paddingTop: 150 }}>
         <h1>Congrats !</h1>
-        <h3>Finished with {this.props.turnNo - 1} turns</h3>
+        <h3>Finished with {this.result} turns</h3>
         <div>
-          <button className='btn btn-warning' onClick={this.props.restart}>Restart</button>
+          <button className='btn btn-warning' onClick={this.restart}>Restart</button>
         </div>
       </div>
     );
   }
 }
+
+export default withRouter(ResultView);
