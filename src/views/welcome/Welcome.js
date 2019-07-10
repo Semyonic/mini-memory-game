cb6d401d20d20ee94cffbdbe9cfc9e79d3bb725d
Semih Onay <6043982+Semyonic@users.noreply.github.com>
2019-07-10 10:22:24 +0300

    Update Welcome.js

--- src/views/welcome/Welcome.js
+++ src/views/welcome/Welcome.js
@@ -1,19 +1,23 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { withRouter } from 'react-router-dom';
 
-export default class WelcomeView extends React.Component {
+class WelcomeView extends React.Component {
   constructor(props) {
     super(props);
     this.onClick = this.onClick.bind(this);
   }
 
-  onClick() {}
+  onClick() {
+    this.props.history.push('/play');
+  }
 
   render() {
     return (
-      <Link to="/play">
-        <h1 style={{ textAlign: "center", paddingTop: '150px' }}>Play</h1>
-      </Link>
+      <button onClick={this.onClick} style={{ marginTop: '40%', marginLeft: '30%', width: 150, height: 50 }}>
+        Play
+      </button>
     );
   }
 }
+
+export default withRouter(WelcomeView);
